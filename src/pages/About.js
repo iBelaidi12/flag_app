4051458acc1dc8622bab48adbce4c106a100211f
ImadKho <96742542+ImadKho@users.noreply.github.com>
2024-06-17 20:51:14 +0100

    about page dark theme

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import { useContext } from 'react';
-import { ThemeContext } from '../App';
-import '../styles/About.css'
+import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+import "../styles/About.css";
+import crossedFlagsOrange from "./continents/crossed-flags-orange.svg";
+import crossedFlagsBlue from "./continents/crossed-flags-blue.svg";
 
 export const About = () => {
-  const {darkTheme} = useContext(ThemeContext);
-
+  const { darkTheme } = useContext(ThemeContext);
 
   return (
-    <div className={darkTheme ? "about-container dark" : "about-container light"}>
-        <div className="inner-container">
-          <h1>About the app</h1>
+    <div
+      className={darkTheme ? "about-container dark" : "about-container light"}
+    >
+      <h1>
+        About <strong>the app</strong>
+      </h1>
+      <div className="inner-container">
+        <div className="hold-flag">
+          <img src={darkTheme ? crossedFlagsOrange : crossedFlagsBlue} alt="" />
         </div>
+        <p className="about-text">
+          What if you get challenged by your friends in a{" "}
+          <strong>flag context</strong> ? No worries ! Thank to this <strong>React</strong> application
+          you will be able to improve your flags knowledge by revising the known ones and searching for the
+          <strong> flags</strong> that still confuse you. Prepare yourself and get ready to show off while facing the strongest opponents ! <br />
+          <strong>Ramy</strong> and <strong>Nada</strong> may need it for their next dual ...
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
